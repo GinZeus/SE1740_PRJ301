5c9672047d8a307113aa456c2770183d6358c065
GinZeus <datnguyen2572003@gmail.com>
2023-06-05 08:56:02 +0700

    product model

--- WebBanKinh/src/java/model/Product.java
+++ WebBanKinh/src/java/model/Product.java
@@ -9,5 +9,82 @@ package model;
  * @author datng
  */
 public class Product {
+    private String id;
+    private String name;
+    private String image;
+    private double price;
+    private String color;
+    private String material;
+    private String type;
+
+    public Product() {
+    }
+
+    public Product(String id, String name, String image, double price, String color, String material, String type) {
+        this.id = id;
+        this.name = name;
+        this.image = image;
+        this.price = price;
+        this.color = color;
+        this.material = material;
+        this.type = type;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getMaterial() {
+        return material;
+    }
+
+    public void setMaterial(String material) {
+        this.material = material;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+    
     
 }
